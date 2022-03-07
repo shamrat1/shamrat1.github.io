920c904e70c3c4d63967712d0933d3b169712011
shamrat1 <yshamrat@gmail.com>
2022-03-07 10:53:35 +0600

    JD added

--- js/shamrat.js
+++ js/shamrat.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var joiningDate = "October, 1, 2018";
+    var joiningDate = "May, 1, 2019";
 
     var careerStarted = new Date(joiningDate);
     var today = new Date();
